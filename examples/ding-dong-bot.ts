9ca7a312004dba6946bfe87d1f85531dc8289fd7
梨子(Riko) <18373361+rikakomoe@users.noreply.github.com>
2020-01-18 13:48:54 +0800

    Update ding-dong-bot.ts
    It's better to clearly instruct new users that outgoing messages will cause a 'message' event emit in order to prevent infinitely sending messages

--- examples/ding-dong-bot.ts
+++ examples/ding-dong-bot.ts
@@ -112,6 +112,11 @@ function onError (e: Error) {
  */
 async function onMessage (msg: Message) {
   console.info(msg.toString())
+  
+  if (msg.self()) {
+    console.info('Message discarded because its outgoing')
+    return
+  }
 
   if (msg.age() > 2 * 60) {
     console.info('Message discarded because its TOO OLD(than 2 minutes)')
@@ -120,7 +125,6 @@ async function onMessage (msg: Message) {
 
   if (msg.type() !== bot.Message.Type.Text
     || !/^(ding|ping|bing|code)$/i.test(msg.text())
-    /* && !msg.self() */
   ) {
     console.info('Message discarded because it does not match ding/ping/bing/code')
     return
